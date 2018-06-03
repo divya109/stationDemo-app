706cf81553745cacab8aa757d261422bef7836d0
Divya Chepuri <divyarao025@gmail.com>
2018-06-03 14:32:53 -0400

    Added API Integration Testing

--- src/test/java/com/iheartradio/stationdemo/StationdemoApplicationTests.java
+++ src/test/java/com/iheartradio/stationdemo/StationdemoApplicationTests.java
@@ -6,6 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.Assertions.assertThat;
 import com.iheartradio.stationdemo.model.Station;
@@ -34,5 +39,18 @@ public class StationdemoApplicationTests {
 		assertThat(testName).isEqualTo(station.getName());
 	}
 	
+	TestRestTemplate restTemplate = new TestRestTemplate();
 
+	HttpHeaders headers = new HttpHeaders();
+
+	@Test
+	public void testRetrieveStudentCourse() {
+		String stationURL = "http://localhost:8080/stations/101";
+		String expectedStationName = "KISSFM";
+		
+		HttpEntity<String> entity = new HttpEntity<String>(null, headers);
+		
+		ResponseEntity<Station> response = restTemplate.exchange(stationURL,HttpMethod.GET,entity, Station.class);
+		assertThat(expectedStationName).isEqualTo(response.getBody().getName());
+	}
 }
