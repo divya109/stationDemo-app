75f15c4b21a9767fc7296d376fdc8e1f7576551d
Divya Chepuri <divyarao025@gmail.com>
2018-06-02 22:35:55 -0400

    First Commit

---
+++ src/main/java/com/iheartradio/stationdemo/StationdemoApplication.java
@@ -0,0 +1,12 @@
+package com.iheartradio.stationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StationdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StationdemoApplication.class, args);
+	}
+}
