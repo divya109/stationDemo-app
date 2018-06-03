52902027eb977818c339f4cdf0ffe495f60b2c55
Divya Chepuri <divyarao025@gmail.com>
2018-06-03 13:02:49 -0400

    Data Integration Tests

--- src/test/java/com/iheartradio/stationdemo/StationdemoApplicationTests.java
+++ src/test/java/com/iheartradio/stationdemo/StationdemoApplicationTests.java
@@ -2,15 +2,37 @@ package com.iheartradio.stationdemo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.assertThat;
+import com.iheartradio.stationdemo.model.Station;
+import com.iheartradio.stationdemo.repository.StationRepository;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DataJpaTest
 public class StationdemoApplicationTests {
 
+	@Autowired
+	StationRepository stationRepository;
+	
+	@Autowired
+	TestEntityManager entityManager;
+	
 	@Test
 	public void contextLoads() {
 	}
+	
+	@Test
+	public void whenFindStationByName_thenReturnStation(){
+		String testName = "testStation";
+	    entityManager.persist(new Station(testName,"FM","testing"));
+		Station station = stationRepository.findByName(testName);
+		assertThat(testName).isEqualTo(station.getName());
+	}
+	
 
 }
