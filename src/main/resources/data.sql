40962186bb7fb398fa071914534264c9f8aa06f9
Divya Chepuri <divyarao025@gmail.com>
2018-06-03 10:56:46 -0400

    Inserting data into station table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+insert into station(ID,DESCRIPTION,FREQUENCY,LOCATION,LOGO_URL,NAME,NOW_PLAYING,TRANSMISSION_MODE) 
+values(101,'The hit music station',101.2,'Austin,TX','https://i.iheart.com/v3/re/assets/images/2173.png','KISSFM','song1..','FM');
+
+insert into station(ID,DESCRIPTION,FREQUENCY,LOCATION,LOGO_URL,NAME,NOW_PLAYING,TRANSMISSION_MODE) 
+values(102,'The hit music station',101.3,'Austin,TX','https://i.iheart.com/v3/re/assets/images/2173.png','REAL','song2..','FM');
+
+insert into station(ID,DESCRIPTION,FREQUENCY,LOCATION,LOGO_URL,NAME,NOW_PLAYING,TRANSMISSION_MODE) 
+values(103,'The hit music station',101.4,'Austin,TX','https://i.iheart.com/v3/re/assets/images/2173.png','KOST','song3..','FM');
+
+insert into station(ID,DESCRIPTION,FREQUENCY,LOCATION,LOGO_URL,NAME,NOW_PLAYING,TRANSMISSION_MODE) 
+values(104,'The hit music station',101.5,'Austin,TX','https://i.iheart.com/v3/re/assets/images/2173.png','KISSFM','song4..','FM');
+
+insert into station(ID,DESCRIPTION,FREQUENCY,LOCATION,LOGO_URL,NAME,NOW_PLAYING,TRANSMISSION_MODE) 
+values(105,'The hit music station',101.6,'Austin,TX','https://i.iheart.com/v3/re/assets/images/2173.png','ALT','song5','FM');
+
+insert into station(ID,DESCRIPTION,FREQUENCY,LOCATION,LOGO_URL,NAME,NOW_PLAYING,TRANSMISSION_MODE) 
+values(106,'The hit music station',101.7,'Austin,TX','https://i.iheart.com/v3/re/assets/images/2173.png','KVET','song6','FM');
